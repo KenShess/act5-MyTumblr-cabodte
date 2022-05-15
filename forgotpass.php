9edbc9a647c0bc8f93c3370b67e5a68672e96635
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 19:52:38 +0800

    Profile Page and Images added

--- forgotpass.php
+++ forgotpass.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>MyTumblr Login</title>
+  	<title>Forgot Password</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
