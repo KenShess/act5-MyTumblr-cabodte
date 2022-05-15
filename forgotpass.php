8aad2985992391051c95100a4e41ea5c736c1c5b
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 19:32:45 +0800

    Forgot Password added

---
+++ forgotpass.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>MyTumblr Login</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet">
+	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 col-lg-5">
+					<div class="login-wrap p-4 p-md-5">
+
+		      	<h3 class="text-md-left mb-4">Find Your Account</h3>
+						
+						<form action="#" class="login-form">
+		      		<div class="form-group">
+		      			<input type="text" class="form-control rounded-left" placeholder="Email" required>
+		      		</div>
+	            <div class="form-group d-md-flex">
+								<div class="w-50 text-md-left">
+									<a href="index.php">Back</a>
+								</div>
+
+								<div class="w-50 text-md-right">
+									<a href="#">Search</a>
+								</div>
+	            </div>
+	          </form>
+	        </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+  <!--<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script> -->
+
+	</body>
+</html>
+
