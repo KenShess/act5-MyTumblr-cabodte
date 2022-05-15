9c30da0fc8d1af7dd1e355a67837d409616223fb
CabodteKenneth <kennethcabodte@gmail.com>
2022-05-15 19:54:35 +0800

    Photos added

---
+++ photos.php
@@ -0,0 +1,61 @@
+<!doctype html>
+                        <html>
+                            <head>
+                                <meta charset='utf-8'>
+                                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                                <title>Photos</title>
+
+                                <link href='css/bootstrap.min.css' rel='stylesheet'>
+
+                                <script type='text/javascript' src='js/jquery.min.js'></script>
+
+                                <style>.profile-head {
+    transform: translateY(5rem)
+}
+.cover {
+}
+
+body {
+    background: #654ea3;
+    background: linear-gradient(to right, #e96443, #904e95);
+    min-height: 100vh;
+    overflow-x: hidden
+}</style>
+                                </head>
+                                <body oncontextmenu='return false' class='snippet-body'>
+                                <div class="row py-5 px-4">
+    <div class="col-md-5 mx-auto">
+        <!-- Profile widget -->
+        <div class="bg-white shadow rounded overflow-hidden">
+            
+
+            <div class="py-4 px-4">
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    <h5 class="mb-0">PHOTOS</h5>
+                </div>
+                <div class="row">
+                    
+                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-profile.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-profile-background.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 mb-2 pr-lg-1"><img src="images/photo-1.jpg" alt="" class="img-fluid rounded shadow-sm"></div>              
+                    <div class="col-lg-6 mb-2 pl-lg-1"><img src="images/photo-2.jpg" alt="" class="img-fluid rounded shadow-sm"></div>                
+                    <div class="col-lg-6 pr-lg-1 mb-2"><img src="images/photo-3.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
+                    <div class="col-lg-6 pl-lg-1"><img src="images/photo-4.jpg" alt="" class="img-fluid rounded shadow-sm"></div>
+                    
+                </div> 
+            </div>
+
+            <div class="px-4 py-3">
+                <div class="p-4 rounded shadow-sm bg-light">
+                    <p class="font-italic mb-0">All Rights Reserved. Copyright 2022</p>
+                </div>
+            </div>
+
+
+
+        </div>
+    </div>
+</div>
+                                <script type='text/javascript' src='js/bootstrap.bundle.min.js'></script>
+                                </body>
+                            </html>
